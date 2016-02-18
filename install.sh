629a1a4b96edc927f07ac2352ba93f771c23892a
Phil Huang <pichuang@cs.nctu.edu.tw>
2016-02-19 02:05:44 +0800

    init
    Signed-off-by: Phil Huang <pichuang@cs.nctu.edu.tw>

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#TODO Support more Linux Distribution
+
+# For Ubuntu/Debian
+apt-get update
+apt-get install -y software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
